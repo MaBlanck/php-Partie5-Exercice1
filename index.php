d6835774cf34a5c59650db7e46f5b6d2dc4c9286
Max <maxblanck@outlook.fr>
2020-12-09 11:12:00 +0100

    php Partie 5  Exercice 1

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php 
+    $months = array('janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre');
+?>
+
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Exercice 1 Partie 5</title>
+</head>
+<body>
+    <h1>Exercice 1 Partie 5</h1>
+    <p><?= $months[11] ?></p>
+</body>
+</html>
